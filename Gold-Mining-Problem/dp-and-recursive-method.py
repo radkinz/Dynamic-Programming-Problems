e907a0fe74c73c3bea39d8d013eac1c00fa7e609
Ruby Adkins <81705278+radkinz@users.noreply.github.com>
2021-05-23 00:04:09 -0400

    Update and rename dp.py to dp-and-recursive-method.py

--- Gold-Mining-Problem/dp-and-recursive-method.py
+++ Gold-Mining-Problem/dp-and-recursive-method.py
@@ -1,5 +1,19 @@
 goldmine = [[1, 3, 6, 7, 5], [8, 7, 2, 3, 4], [2, 4, 5, 8, 7], [6, 3, 7, 9, 1]]
 
+#recursion
+def goldMine(mine, startingpos):
+  #base case
+  if len(mine) == 1:
+    return mine[0][startingpos]
+ 
+  if startingpos == 0:
+    return  mine[0][startingpos] + max(goldMine(mine[1:],startingpos), goldMine(mine[1:],startingpos+1))
+  if startingpos == len(mine[0])-1:
+    return  mine[0][startingpos] + max(goldMine(mine[1:],startingpos), goldMine(mine[1:],startingpos-1))
+
+  return  mine[0][startingpos] + max(goldMine(mine[1:], startingpos), goldMine(mine[1:],startingpos+1), goldMine(mine[1:],startingpos-1))
+
+#DP
 def goldmineDP(mine):
   table = [] 
   for i in range(len(mine)):
@@ -22,4 +36,4 @@ def goldmineDP(mine):
   
   return max(table[len(mine)-1])
 
-print(goldmineDP(goldmine))
+print(goldmineDP(goldmine), goldMine(goldmine, len(goldmine)-1))
